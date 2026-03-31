5f7a1957b4814694dd342aa74b3b6eaa4f8545bd
Aryan-ssg <901aryan@gmail.com>
2026-03-31 14:24:04 +0530

    revisited TowerOfHanoi problem

--- TowerHanoi.java
+++ TowerHanoi.java
@@ -11,6 +11,22 @@ public class TowerHanoi {
     }
 
     public static void main(String[] args) {
-        TOH(3, 'A', 'B', 'C');
+        // TOH(3, 'A', 'B', 'C');
+        towerOfHanoi(3, 'A', 'B', 'C');
+
+    }
+
+
+    public static void towerOfHanoi(int n , char src,char helper,char des){
+
+        if(n==1){
+            System.out.println("Moved "+n+" from "+src+" to "+des);
+            return;
+        }
+
+        towerOfHanoi(n-1, src, des, helper);
+        System.out.println("Moved "+n+" from "+src+" to "+des);
+        towerOfHanoi(n-1, helper, src, des);
+
     }
 }
