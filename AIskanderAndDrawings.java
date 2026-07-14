0db1e4a7e533c842c5c8e7fac0a844d407248e9c
Aryan-ssg <901aryan@gmail.com>
2026-07-15 00:29:17 +0530

    Div 3 contest

---
+++ AIskanderAndDrawings.java
@@ -0,0 +1,193 @@
+import java.io.*;
+import java.util.*;
+
+public class AIskanderAndDrawings {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
+        int tc = nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        while (tc-- > 0) {
+            
+            int n=nextInt();
+            String s=nextLine();
+            int maxline=0;
+            int currmax=0;
+            for(int i=0;i<n;i++){
+                if(s.charAt(i)=='*'){
+                    maxline=Math.max(maxline,currmax);
+                    currmax=0;
+                }else{
+                    currmax++;
+                }
+            }
+            maxline=Math.max(currmax,maxline);
+
+            int ans=Math.ceilDiv(maxline, 2);
+            sb.append(ans+"\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine();
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+
+    static long lcm(long a, long b) {
+        return Math.abs(a / gcd(a, b) * b);
+    }
+
+    static long modPow(long base, long exp, long mod) {
+        long result = 1;
+        base %= mod;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+
+        return result;
+    }
+
+    static long modAdd(long a, long b, long mod) {
+        return ((a % mod) + (b % mod)) % mod;
+    }
+
+    static long modSub(long a, long b, long mod) {
+        return ((a % mod) - (b % mod) + mod) % mod;
+    }
+
+    static long modMul(long a, long b, long mod) {
+        return ((a % mod) * (b % mod)) % mod;
+    }
+
+    static boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+
+        if (n >= 0) isPrime[0] = false;
+        if (n >= 1) isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+        return isPrime;
+    }
+
+    static int[] smallestPrimeFactor(int n) {
+        int[] spf = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+            spf[i] = i;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (spf[i] == i) {
+                for (int j = i * i; j <= n; j += i) {
+                    if (spf[j] == j)
+                        spf[j] = i;
+                }
+            }
+        }
+
+        return spf;
+    }
+
+    static List<Integer> primeFactors(int n, int[] spf) {
+        List<Integer> factors = new ArrayList<>();
+
+        while (n > 1) {
+            factors.add(spf[n]);
+            n /= spf[n];
+        }
+
+        return factors;
+    }
+
+    static List<Integer> divisors(int n) {
+        List<Integer> divs = new ArrayList<>();
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                divs.add(i);
+
+                if (i != n / i)
+                    divs.add(n / i);
+            }
+        }
+
+        Collections.sort(divs);
+        return divs;
+    }
+
+    static int countDivisors(int n, int[] spf) {
+        int ans = 1;
+
+        while (n > 1) {
+            int prime = spf[n];
+            int cnt = 0;
+
+            while (n > 1 && spf[n] == prime) {
+                cnt++;
+                n /= prime;
+            }
+
+            ans *= (cnt + 1);
+        }
+
+        return ans;
+    }
+
+    static int phi(int n, int[] spf) {
+        int result = n;
+
+        while (n > 1) {
+            int prime = spf[n];
+            result -= result / prime;
+
+            while (n > 1 && spf[n] == prime)
+                n /= prime;
+        }
+
+        return result;
+    }
+}
---
+++ EMashaAndTheGarland.java
@@ -0,0 +1,247 @@
+import java.io.*;
+import java.util.*;
+
+public class EMashaAndTheGarland {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
+        int tc = nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        while (tc-- > 0) {
+            int n = nextInt();
+            int oper = nextInt();
+            String s = nextLine();
+
+            int[] firstzero = new int[n + 2];
+            int[] firstone = new int[n + 2];
+
+            for (int i = 1; i <= n; i++) {
+                int bit = s.charAt(i - 1) - '0';
+
+                int desired0 = (i - 1) & 1;
+                int desired1 = i & 1;
+                firstzero[i] = bit ^ desired0;
+                firstone[i] = bit ^ desired1;
+            }
+
+
+            long[] preOnes0 = new long[n + 2];
+
+
+            long[] preOnes1 = new long[n + 2];
+          
+            long[] prePairs0 = new long[n + 2];
+
+
+            long[] prePairs1 = new long[n + 2];
+
+            for (int i = 1; i <= n; i++) {
+                preOnes0[i] = preOnes0[i - 1] + firstzero[i];
+                preOnes1[i] = preOnes1[i - 1] + firstone[i];
+
+                if (i >= 2) {
+                    prePairs0[i] = prePairs0[i - 1] + (firstzero[i - 1] & firstzero[i]);
+                    prePairs1[i] = prePairs1[i - 1] + (firstone[i - 1] & firstone[i]);
+                } 
+                
+                else {
+                    prePairs0[i] = 0;
+                    prePairs1[i] = 0;
+                }
+            }
+            while (oper-- > 0) {
+                
+                int l = nextInt();
+                int r = nextInt();
+                int k = nextInt();
+
+                long ones0 = preOnes0[r] - preOnes0[l - 1];
+                long pairs0 = prePairs0[r] - prePairs0[l]; 
+
+                long blocks0 = ones0 - pairs0;
+
+     
+                long ones1 = preOnes1[r] - preOnes1[l - 1];
+                long pairs1 = prePairs1[r] - prePairs1[l];
+                
+                long blocks1 = ones1 - pairs1;
+
+                if (Math.min(blocks0, blocks1) <= k)
+                   sb.append("YES\n");
+                else
+                    sb.append("NO\n");
+            }
+
+
+
+        }
+
+    
+
+    System.out.print(sb);
+
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine();
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+
+    static long lcm(long a, long b) {
+        return Math.abs(a / gcd(a, b) * b);
+    }
+
+    static long modPow(long base, long exp, long mod) {
+        long result = 1;
+        base %= mod;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+
+        return result;
+    }
+
+    static long modAdd(long a, long b, long mod) {
+        return ((a % mod) + (b % mod)) % mod;
+    }
+
+    static long modSub(long a, long b, long mod) {
+        return ((a % mod) - (b % mod) + mod) % mod;
+    }
+
+    static long modMul(long a, long b, long mod) {
+        return ((a % mod) * (b % mod)) % mod;
+    }
+
+    static boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+
+        if (n >= 0)
+            isPrime[0] = false;
+        if (n >= 1)
+            isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+        return isPrime;
+    }
+
+    static int[] smallestPrimeFactor(int n) {
+        int[] spf = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+            spf[i] = i;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (spf[i] == i) {
+                for (int j = i * i; j <= n; j += i) {
+                    if (spf[j] == j)
+                        spf[j] = i;
+                }
+            }
+        }
+
+        return spf;
+    }
+
+    static List<Integer> primeFactors(int n, int[] spf) {
+        List<Integer> factors = new ArrayList<>();
+
+        while (n > 1) {
+            factors.add(spf[n]);
+            n /= spf[n];
+        }
+
+        return factors;
+    }
+
+    static List<Integer> divisors(int n) {
+        List<Integer> divs = new ArrayList<>();
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                divs.add(i);
+
+                if (i != n / i)
+                    divs.add(n / i);
+            }
+        }
+
+        Collections.sort(divs);
+        return divs;
+    }
+
+    static int countDivisors(int n, int[] spf) {
+        int ans = 1;
+
+        while (n > 1) {
+            int prime = spf[n];
+            int cnt = 0;
+
+            while (n > 1 && spf[n] == prime) {
+                cnt++;
+                n /= prime;
+            }
+
+            ans *= (cnt + 1);
+        }
+
+        return ans;
+    }
+
+    static int phi(int n, int[] spf) {
+        int result = n;
+
+        while (n > 1) {
+            int prime = spf[n];
+            result -= result / prime;
+
+            while (n > 1 && spf[n] == prime)
+                n /= prime;
+        }
+
+        return result;
+    }
+}
